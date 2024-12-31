1cefdcbde2168ce0d305208c41d0feb424876e0a
gfq40 <66170695+gfq40@users.noreply.github.com>
2024-12-31 09:32:09 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'ultrasky-pwa-v1';
+const ASSETS_TO_CACHE = [
+  './',            // la root
+  './index.html',  // file principali
+  './manifest.json',
+  // ... e tutti i file css/js/img che vuoi in cache
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
