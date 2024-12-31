b948f7bd4be49691dfee0e85d2c3eb41169a2f65
gfq40 <66170695+gfq40@users.noreply.github.com>
2024-12-31 10:06:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,45 @@
-const CACHE_NAME = 'ultrasky-pwa-v1';
-const ASSETS_TO_CACHE = [
-  './',            // la root
-  './index.html',  // file principali
-  './manifest.json',
-  // ... e tutti i file css/js/img che vuoi in cache
+const CACHE_NAME = 'ultrasky-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/main.js',
+  '/icons/logo.png',
+  '/icons/logo.png'
 ];
 
-// Install
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-// Activate
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
 self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(keys => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
           }
         })
       );
-    })
-  );
-});
-
-// Fetch
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
     })
   );
 });
